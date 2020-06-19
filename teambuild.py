016840051ec9ef5dbfc1a4c9cd72f83cea52c21e
RakuTheSenpai <raul.r.h.rosas@gmail.com>
2020-06-19 12:39:42 -0700

    Finished

--- teambuild.py
+++ teambuild.py
@@ -1,10 +1,30 @@
 import discord
+from discord.ext import commands
 
 TOKEN =''
-client = discord.Client()
+
+client = commands.Bot(command_prefix='!')
+playerPool = []
+
+@client.command()
+async def removeplayer(ctx,arg):
+    playerPool.remove(arg)
+    await ctx.send('Removed player '+arg)
+
+@client.command()
+async def addplayer(ctx,arg):
+    playerPool.append(arg)
+    await ctx.send('Added player '+arg)
+
+@client.command()
+async def showplayers(ctx):
+    await ctx.send('Players: '+', '.join(playerPool))
 
 @client.event
 async def on_ready():
     print('Bot online. Connected as {0.user}'.format(client))
 
 client.run(TOKEN)
+
+
+
