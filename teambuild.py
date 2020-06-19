c214bebd46965231322d4692238dbc021030aac2
RakuTheSenpai <raul.r.h.rosas@gmail.com>
2020-06-19 11:48:21 -0700

    Update teambuild.py

--- teambuild.py
+++ teambuild.py
@@ -1,9 +1,10 @@
 import discord
 
+TOKEN =''
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print('Bot online. Connected as {0.user}'.format(client))
 
-client.run('NzIzNjA1NDA1NTc3NjQyMDI0.Xu0ExQ.RgmHz8c99i5o8ZJ_DuQbzM-wDbw')
+client.run(TOKEN)
